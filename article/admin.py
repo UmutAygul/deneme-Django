bb0d354ff6b7b495c342682a3db7e5bebcee780c
Umut Aygul <uumutaygul@gmail.com>
2022-08-02 19:13:04 +0300

    renk?

--- article/admin.py
+++ article/admin.py
@@ -7,7 +7,12 @@ from .models import Article
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display= ["title","author","created_date"]
+    
     list_display_links= ["title","created_date"]
+    
+    search_fields = ["title"]
+    
+    list_filter=["created_date"]
     
     class Meta:
         model=Article
--- article/views.py
+++ article/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+#url geldiğinde calıstırılacak fonksyionlar
+
+def index(request):
+    return render(request,"article/index.html")
+
+
+
+
