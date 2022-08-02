2d4050d4b50e5419c4e5587ef1a953bc47d35d6d
Umut Aygul <uumutaygul@gmail.com>
2022-08-02 19:42:26 +0300

    hi

--- article/views.py
+++ article/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render,HttpResponse
 #url geldiğinde calıstırılacak fonksyionlar
 
 def index(request):
-    return render(request,"article/index.html")
+    return render(request,"index.html")
 
 
 
