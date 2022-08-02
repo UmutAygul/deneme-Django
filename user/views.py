b22e0b343c63898b9bd40d2fbbc8da39cd48ccef
Umut Aygul <uumutaygul@gmail.com>
2022-08-03 02:27:11 +0300

    til-regis

--- user/views.py
+++ user/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 
 def register(request):
-    return render(request,"register.html")
+    form=forms.RegisterForm()
+    
+    context={
+        "form":form
+    }
+    
+    return render(request,"register.html",context)
+
 def login(request):
     return render(request,"login.html")
 def logout(request):
