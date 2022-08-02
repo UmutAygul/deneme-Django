3caddbc2a2f6c4c0f8776baf2128115aa97ea893
Umut Aygul <uumutaygul@gmail.com>
2022-08-02 17:56:10 +0300

    admin set

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+
+#admin panelindeki article gözükecek
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display= ["title","author","created_date"]
+    list_display_links= ["title","created_date"]
+    
+    class Meta:
+        model=Article
+
+
