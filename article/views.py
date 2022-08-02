55550dff438c3baae40b9373336d839460c3f420
Umut Aygul <uumutaygul@gmail.com>
2022-08-03 00:41:29 +0300

    register

--- article/views.py
+++ article/views.py
@@ -4,9 +4,14 @@ from django.shortcuts import render,HttpResponse
 #url geldiğinde calıstırılacak fonksyionlar
 
 def index(request):
-    return render(request,"index.html")
-
+    context={
+        "numbers":[1,2,3,412,412,3415,12],
+    }
+    
+    return render(request,"index.html",context)
 def about(request):
     return render(request,"about.html")
 
 
+def detail(request,id):
+    return HttpResponse("Detail:" +str(id))
