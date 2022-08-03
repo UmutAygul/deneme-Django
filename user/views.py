695d8093b47cb17affcdafaa4e07729b6428a415
Umut Aygul <uumutaygul@gmail.com>
2022-08-03 20:50:40 +0300

    registeration to home

--- user/views.py
+++ user/views.py
@@ -1,16 +1,61 @@
-from django.shortcuts import render
-from . import forms
-
+from django.shortcuts import render,redirect
+# from .forms import forms
+from .forms import RegisterForm
 # Create your views here.
+from django.contrib.auth.models import User
+
+from django.contrib.auth import login
 
 def register(request):
-    form=forms.RegisterForm()
     
-    context={
+    if request.method == "POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get("username")
+            password= form.cleaned_data.get("password")
+            
+            newUser = User(username=username)
+            newUser.set_password(password)
+            
+            newUser.save()
+            
+            login(request)
+            
+            return redirect("index")
+        context={
         "form":form
     }
-    
+            
+    else:
+        form=RegisterForm()
+        context={
+        "form":form
+    }  
     return render(request,"register.html",context)
+
+    # if form.is_valid():
+    #     username= form.cleaned_data.get("username")
+    #     password= form.cleaned_data.get("password")
+        
+    #     newUser=User(username=username)
+    #     newUser.set_password(password)
+        
+    #     newUser.save()
+    #     login(request,newUser)
+    #     return redirect("index")
+    
+    # context={
+    #     "form":form
+    # }  
+    # return render(request,"register.html",context)
+    
+    # else:
+    #     form=RegisterForm()
+    #     context={
+    #     "form":form
+    # }  
+    #     return render(request,"register.html",context)
+
 
 def login(request):
     return render(request,"login.html")
